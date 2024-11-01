47f11ee3c5d288efb3027a1e88bfdc2671972ee1
tongkpmarcus <151003624+tongkpmarcus@users.noreply.github.com>
2024-11-01 18:38:42 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -110,7 +110,11 @@ if form.form_submit_button("Submit"):
         st.header("Response_emb:")
         st.write(response_emb)
 
+st.expander("IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
 
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+Always consult with qualified professionals for accurate and personalized advice.")
 
 
 
