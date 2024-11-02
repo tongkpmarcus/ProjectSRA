d93d21c021c4504b1aa2cdc3b3f7ada6333bf127
tongkpmarcus <151003624+tongkpmarcus@users.noreply.github.com>
2024-11-02 23:21:26 +0800

    Update 1_About Us.py

--- pages/1_About Us.py
+++ pages/1_About Us.py
@@ -5,6 +5,16 @@ st.set_page_config(
     page_icon="✈️"
 )
 st.title("About Us") 
+st.markdown(
+    """
+    Welcome to Project SRA(ProSRA), where we are dedicated to simplifying safety risk assessments for everyone.\n
+    Our mission is to make the reviewing process efficient and straightforward, \n
+    empowering everyone to identify and manage risks with confidence and ease. \n
+    By streamlining previous cross-examining processes for risk assessment, \n
+    we now provide a user-friendly interface that lists all necessary information for SRA, \n
+    all in one place. 
 
+    """
+)
 st.title("This is the about us")
 st.sidebar.success("You are currently viewing About Us")
