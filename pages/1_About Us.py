a3cc68994d4e2a442b5654ecae93b452bcad269c
tongkpmarcus <151003624+tongkpmarcus@users.noreply.github.com>
2024-11-02 16:27:14 +0800

    Add files via upload

---
+++ pages/1_About Us.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("About Us") 
+
+st.title("This is the about us")
+st.sidebar.success("You are currently viewing About Us")
---
+++ pages/2_Methodology.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("About Us") 
+
+st.title("This is the Methodology")
+st.sidebar.success("You are currently viewing Methodology")
