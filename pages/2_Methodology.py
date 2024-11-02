2eb418baf4763d6052604b3ebb24fe5bc9d6266e
tongkpmarcus <151003624+tongkpmarcus@users.noreply.github.com>
2024-11-03 00:02:46 +0800

    Update 2_Methodology.py

--- pages/2_Methodology.py
+++ pages/2_Methodology.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+st.set_page_config(
+    layout="centered",
+    page_title="Project SRA",
+    page_icon="✈️"
+)
 st.title("This is the Methodology")
+
+st.image("methodology2.jpg")
+
 st.sidebar.success("You are currently viewing Methodology")
