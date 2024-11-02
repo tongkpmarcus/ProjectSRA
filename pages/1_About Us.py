3ce6e122e28ded96ec118fce0ed7bce9d88001bf
tongkpmarcus <151003624+tongkpmarcus@users.noreply.github.com>
2024-11-02 23:20:09 +0800

    Update 1_About Us.py

--- pages/1_About Us.py
+++ pages/1_About Us.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+st.set_page_config(
+    layout="centered",
+    page_title="Project SRA",
+    page_icon="✈️"
+)
 st.title("About Us") 
 
 st.title("This is the about us")
